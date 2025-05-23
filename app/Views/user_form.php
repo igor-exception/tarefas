d9fcc5a947cb960b22687d2ccd52e6ee7d41b035
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2025-05-23 15:23:54 -0300

    Estilizando tela de cadastro de usuario

--- app/Views/user_form.php
+++ app/Views/user_form.php
@@ -1,18 +1,47 @@
-<?= view('layouts/header') ?>
-    <h1>Cadastrar Novo Usuário</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Sistema de Tarefas</title>
+</head>
+<body>
+    <div class="container d-flex flex-column justify-content-center align-items-center vh-100">
 
-    <?= view('layouts/alerts') ?>
+        <div class="text-center mb-4">
+            <h1 class="display-6">Sistema de Tarefas</h1>
+            <p class="lead">Para prosseguir, crie uma conta.<br>Caso já tenha, faça login clicando <a href="<?= base_url('login') ?>">clique aqui</a></p>
+        </div>
 
-    <form method="post" action="/user/store">
-        <label for="username">Username:</label><br>
-        <input type="text" name="username" required><br><br>
+        <div class="col-md-6">
+            <?= view('layouts/alerts') ?>
 
-        <label for="email">Email:</label><br>
-        <input type="email" name="email" required><br><br>
+            <div class="card shadow">
+                <div class="card-body">
+                    <h4 class="text-center mb-6">Criar nova conta</h4>
 
-        <label for="password">Senha:</label><br>
-        <input type="password" name="password" required><br><br>
+                    <form action="<?= base_url('/user/store')?>" method="post">
+                        <div class="mb-3">
+                            <label for="UsernameControlInput1" class="form-label">Username: </label>
+                            <input type="text" class="form-control" id="UsernameControlInput1" placeholder="John Doe" name="username" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="emailFormControl1" class="form-label">Email: </label>
+                            <input type="email" class="form-control" id="emailFormControl1" rows="3" name="email" placeholder="john.doe@gmail.com" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                            <input type="password" class="form-control" id="inputPassword3" name="password">
+                        </div>
 
-        <button type="submit">Cadastrar</button>
-    </form>
-<?= view('layouts/footer') ?>
+                        <button type="submit" class="btn btn-primary">Cadastrar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
