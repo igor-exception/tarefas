8e5a08b57f1361838e565ac6c556a8402bd9da6c
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2025-05-23 14:53:25 -0300

    Estiliza Dashboard

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,9 +1,14 @@
 <?= view('layouts/header') ?>
     <h1>Bem-vindo, <?= esc(session()->get('username')) ?>!</h1>
 
-    <ul>
-        <li><a href="/tasks">Ver minhas tarefas</a></li>
-        <li><a href="/tasks/create">Criar nova tarefa</a></li>
-        <li><a href="/logout">Sair</a></li>
+    <div class="card" style="width: 18rem;">
+    <div class="card-header">
+        O que deseja fazer?
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"><a href="/tasks">Ver minhas tarefas</a></li>
+        <li class="list-group-item"><a href="/tasks/create">Criar nova tarefa</a></li>
+        <li class="list-group-item"><a href="/logout">Sair</a></li>
     </ul>
+    </div>
 <?= view('layouts/footer') ?>
