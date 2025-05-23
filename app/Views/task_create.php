5588b956210efee30280e69d8a523f6c1c203959
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2025-05-23 15:00:16 -0300

    Estilização Nova tarefa

--- app/Views/task_create.php
+++ app/Views/task_create.php
@@ -2,15 +2,23 @@
 
     <h1>Nova Tarefa</h1>
 
-    <form action="/tasks/store" method="post">
-        <label for="title">Título:</label>
-        <input type="text" name="title" id="title" required><br><br>
+    <form action="<?= base_url('/tasks/store')?>" method="post">
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Titulo: </label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Tarefa 1" name="title" required>
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Descrição: </label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description" placeholder="Descrição da terefa"></textarea>
+        </div>
 
-        <label for="description">Descrição:</label><br>
-        <textarea name="description" id="description"></textarea><br><br>
-
-        <button type="submit">Salvar</button>
+        <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
 
-    <p><a href="/tasks">← Voltar para tarefas</a></p>
+
+
+
+
+
+    <p class="lead"><a href="/tasks">← Voltar para tarefas</a></p>
 <?= view('layouts/footer') ?>
