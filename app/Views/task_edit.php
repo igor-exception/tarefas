ad013d7fe5ef3ef1bfeba7c224f9b282a2f63848
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2025-05-23 15:02:28 -0300

    Estilização da tela Editar tarefa

--- app/Views/task_edit.php
+++ app/Views/task_edit.php
@@ -3,14 +3,19 @@
 
     <form action="<?= base_url('/task/update')?>" method="post">
         <input type="hidden" name="id" id="id" value="<?= esc($task['id'])?>"><br><br>
-        <label for="title">Título:</label>
-        <input type="text" name="title" id="title" value="<?= esc($task['title'])?>" required><br><br>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Titulo: </label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Tarefa 1" name="title" value="<?= esc($task['title'])?>" required>
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Descrição: </label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description" placeholder="Descrição da terefa"><?= esc($task['description'])?></textarea>
+        </div>
 
-        <label for="description">Descrição:</label><br>
-        <textarea name="description" id="description"><?= esc($task['description'])?></textarea><br><br>
-
-        <button type="submit">Atualizar</button>
+        <button type="submit" class="btn btn-primary">Atualizar</button>
     </form>
+
+
 
     <p><a href="/tasks">← Voltar para tarefas</a></p>
 <?= view('layouts/footer') ?>
